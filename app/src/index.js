06e15e5801a2ee2d10d4dfd212fe2eb4064af4f4
Olivier Bourgain <obourgain@gmail.com>
2019-05-17 09:39:15 +0200

    Migration react - Filter on book list

--- app/src/index.js
+++ app/src/index.js
@@ -15,11 +15,26 @@ import './index.css';
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 export const store = createStoreWithMiddleware(reducers);
 
+const rootEl = document.getElementById('root')
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
-    document.getElementById('root'));
+    rootEl
+);
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default
+        ReactDOM.render(
+            <Provider store={store}>
+                <NextApp />
+            </Provider>,
+            rootEl
+        )
+    })
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- app/src/views/booklist.js
+++ app/src/views/booklist.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {Col, Container, Row, Table, Input} from 'reactstrap'
+import {Col, Container, Row, Table, Input, Button} from 'reactstrap'
 
 import Tag from './tag'
 import {fetchBooks} from '../actions/book.action'
@@ -11,12 +11,13 @@ import Pagination from "./tools/pagination";
 class BookList extends Component {
     state = {
         page: 0,
-        size: 20
+        size: 20,
+        term: ''
     }
 
     constructor(props) {
         super(props);
-        if (!this.props.book.list) this.props.fetchBooks(0);
+        if (!this.props.book.list) this.props.fetchBooks(this.state.page, this.state.size, this.state.term);
     }
 
     render() {
@@ -28,62 +29,83 @@ class BookList extends Component {
             return (<Container>Loading...</Container>);
         }
         return (<Container>
-            <Row id="book-list">
-                <h3>Your library</h3>
-                {!book.list && <Row className="col-12">
-                    <Col>No book found</Col>
-                </Row>}
-                {book.list && <>
-                    <Row className="col-12">
-                        <Col className="col-4" id="list-header-summary">
-                            {book.list.size} results
-                            {book.list.totalPages > 1 && <span>
-                                , showing page {book.list.number + 1} of {book.list.totalPages}
-                            </span>}
-                        </Col>
-                        <Col className="col-5">
-                            <Pagination page={book.list.number} nbPages={book.list.totalPages} updatePage={this.updatePage}/>
-                        </Col>
-                        <Col className="col-3">
-                            <span>Show{' '}</span>
-                            <select value={this.state.size} onChange={this.changeSize}>
-                                <option value={10}>10</option>
-                                <option value={20}>20</option>
-                                <option value={50}>50</option>
-                                <option value={100}>100</option>
-                                <option value={1000}>1000</option>
-                            </select>
-                            <span> books</span>
-                        </Col>
+            <Row>
+                <Col className="col-8">
+                    <Row>
+                        <Col className="col-8"><Input type="text" value={this.state.term} onChange={this.updateTerm}/></Col>
+                        <Button type="button" color="success" className="col-2" onClick={this.searchTerm}>Submit</Button>
                     </Row>
-                    <Table bordered striped size="sm">
-                        <thead>
-                        <tr className="row">
-                            <th className="col-5">Title</th>
-                            <th className="col-3">Author</th>
-                            <th className="col-1">Pages</th>
-                            <th className="col-3">Tags</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {book.list.content && book.list.content.map(book => this.renderBook(book))}
-                        </tbody>
-                    </Table>
-                </>}
+                </Col>
+                <Col className="col-4">
+                    <div className="row">
+                        <label htmlFor="showDisc">
+                            <input type="checkbox" id="showDisc" /> Show discarded
+                        </label>
+                    </div>
+                </Col>
             </Row>
+            <h3>Your library</h3>
+            {!book.list && <Row>
+                <Col>No book found</Col>
+            </Row>}
+            {book.list && <>
+                <Row>
+                    <Col className="col-4" id="list-header-summary">
+                        {book.list.size} results
+                        {book.list.totalPages > 1 && <span>
+                            , showing page {book.list.number + 1} of {book.list.totalPages}
+                        </span>}
+                    </Col>
+                    <Col className="col-5">
+                        <Pagination page={book.list.number} nbPages={book.list.totalPages} updatePage={this.updatePage}/>
+                    </Col>
+                    <Col className="col-3">
+                        <span>Show{' '}</span>
+                        <select value={this.state.size} onChange={this.changeSize}>
+                            <option value={10}>10</option>
+                            <option value={20}>20</option>
+                            <option value={50}>50</option>
+                            <option value={100}>100</option>
+                            <option value={1000}>1000</option>
+                        </select>
+                        <span> books</span>
+                    </Col>
+                </Row>
+                <Table bordered striped size="sm">
+                    <thead>
+                    <tr className="row">
+                        <th className="col-5">Title</th>
+                        <th className="col-3">Author</th>
+                        <th className="col-1">Pages</th>
+                        <th className="col-3">Tags</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {book.list.content && book.list.content.map(book => this.renderBook(book))}
+                    </tbody>
+                </Table>
+            </>}
         </Container>);
     }
 
+    searchTerm = () => {
+        this.props.fetchBooks(this.state.page, this.state.size, this.state.term);
+    }
+
+    updateTerm = event => {
+        this.setState({term: event.target.value});
+    }
+
     changeSize = event => {
-        const size = event.target.value;
-        this.setState({ size });
-        this.props.fetchBooks(this.state.page, size);
+        this.setState({ size : event.target.value });
+        this.props.fetchBooks(this.state.page, event.target.value, this.state.term);
     }
 
     updatePage = page => {
         this.setState({ page });
-        this.props.fetchBooks(page, this.state.size);
+        this.props.fetchBooks(page, this.state.size, this.state.term);
     }
+
 
     renderBook(book) {
         return (
