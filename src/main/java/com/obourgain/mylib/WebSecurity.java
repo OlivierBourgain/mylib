01277377a4b55436ad55b48947757b81895849e5
Olivier Bourgain <obourgain@gmail.com>
2020-07-13 22:05:14 +0200

    Delete unecessary java code

--- src/main/java/com/obourgain/mylib/WebSecurity.java
+++ src/main/java/com/obourgain/mylib/WebSecurity.java
@@ -1,20 +1,15 @@
 package com.obourgain.mylib;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableOAuth2Sso
 @Configuration
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/api/**", "/store/**").permitAll()
-                .anyRequest().authenticated()
-                .and().logout().logoutSuccessUrl("/").permitAll();
+        http.authorizeRequests().anyRequest().permitAll();
     }
 }
